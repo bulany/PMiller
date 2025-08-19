ea3f12c5e305ac49f460b08d727ca7032635574a
bulany <bulany.git@gmail.com>
2025-08-19 15:20:47 +0200

    Basic hard coded player with adjustable speed

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <button @click="setSpeed(0.5)">0.5x</button>
+  <button @click="setSpeed(0.75)">0.75x</button>
+  <button @click="setSpeed(1)">1x</button>
+    <video ref="videoRef" controls width="640">
+      <source src="/sample.mp4" type="video/mp4" />
+    </video>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const videoRef = ref<HTMLVideoElement | null>(null)
+
+function setSpeed(rate: number) {
+  if (videoRef.value) videoRef.value.playbackRate = rate
+}
+</script>
