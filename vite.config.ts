6f7585de4b28a9d3b2451f87a05ccee88ec86a17
bulany <bulany.git@gmail.com>
2025-09-14 22:15:57 +0200

    Add PWA support for test and deploy on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['assets/*'],
+      manifest: {
+        name: 'PMiller',
+        short_name: 'PMiller',
+        description: 'PMiller desc goes here',
+        theme_color: '#ffffff'
+      }
+    })
+  ],
 })
